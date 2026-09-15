990d8a476bf01abdb8a55f3fd488d3e8fd0c3ee0
Yu Zhe <2604181@sit.singaporetech.edu.sg>
2026-09-15 16:25:06 +0800

    Values as int

--- auditor.py
+++ auditor.py
@@ -4,3 +4,10 @@ while True:
     user_input = input("Enter stock quantity (or 'quit' to quit): ")
     if user_input == 'quit':
         break
+    if not user_input.isdigit():
+        print("Invalid input. Please enter a valid whole number.")
+        failed += 1
+        continue
+
+    quantity = int(user_input)
+
